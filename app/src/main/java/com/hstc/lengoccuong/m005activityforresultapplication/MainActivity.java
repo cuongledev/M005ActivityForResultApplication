751001c4c6ee1eb14ec023d1e76dd51b77586ec8
cuongle <cuongle.dev@gmail.com>
2019-06-30 17:55:46 +0700

    updated

--- app/src/main/java/com/hstc/lengoccuong/m005activityforresultapplication/MainActivity.java
+++ app/src/main/java/com/hstc/lengoccuong/m005activityforresultapplication/MainActivity.java
@@ -1,14 +1,73 @@
 package com.hstc.lengoccuong.m005activityforresultapplication;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final int REQUEST_CODE_LOGIN = 101;
+    EditText mUsername, mPassword;
+    Button mBtnLogin;
+    TextView mRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        mUsername = findViewById(R.id.username);
+        mPassword = findViewById(R.id.pass);
+        mBtnLogin = findViewById(R.id.login);
+        mRegister = findViewById(R.id.register);
+        mBtnLogin.setOnClickListener(this);
+        mRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.login:
+                break;
+            case R.id.register:
+                doRegister();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void doRegister() {
+        Intent intent = new Intent();
+        intent.setClass(this, RegisterActivity.class);
+        startActivityForResult(intent, REQUEST_CODE_LOGIN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQUEST_CODE_LOGIN:
+
+                if (resultCode==RESULT_OK){
+                    Log.i("TAG_LOGIN","onActivityResult"+ data.getStringExtra(RegisterActivity.KEY_USERNAME) + " - " + data.getStringExtra(RegisterActivity.KEY_PASS));
+                    mUsername.setText(data.getStringExtra(RegisterActivity.KEY_USERNAME));
+                    mPassword.setText(data.getStringExtra(RegisterActivity.KEY_PASS));
+                }
+
+                break;
+            default:
+                break;
+        }
     }
 }
--- app/src/main/java/com/hstc/lengoccuong/m005activityforresultapplication/RegisterActivity.java
+++ app/src/main/java/com/hstc/lengoccuong/m005activityforresultapplication/RegisterActivity.java
@@ -1,6 +1,51 @@
 package com.hstc.lengoccuong.m005activityforresultapplication;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
+    public static final String KEY_USERNAME = "KEY_USERNAME";
+    public static final String KEY_PASS = "KEY_PASS";
+    EditText mUsername,mPass,mRePass;
+    Button mBtnRegister;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+
+        initViews();
+    }
+
+    private void initViews() {
+
+        mUsername = findViewById(R.id.user);
+        mPass = findViewById(R.id.pass);
+        mRePass = findViewById(R.id.re_pass);
+        mBtnRegister = findViewById(R.id.bt_register);
+        mBtnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doSendData();
+            }
+        });
+
+    }
+
+    private void doSendData() {
+
+        Intent intent = new Intent();
+        intent.putExtra(KEY_USERNAME, mUsername.getText().toString());
+        intent.putExtra(KEY_PASS, mPass.getText().toString());
+        setResult(RESULT_OK,intent);
+        Log.i("TAG","doSendData"+ mUsername.getText().toString() + " - " + mPass.getText().toString());
+        finish();
+    }
 }
